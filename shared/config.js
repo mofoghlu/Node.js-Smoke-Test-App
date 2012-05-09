e59281c27674cae017e86538ce815a65fe29e9cf
Mícheál Ó Foghlú <mofoghlu@ofoghlu.net>
2012-05-09 16:45:16 +0200

    Update shared/config.js

--- shared/config.js
+++ shared/config.js
@@ -1,6 +1,7 @@
 var config = {
   cloudData: [
     '',
+    '20120509 @ 15:44',
     'This is data that was returned from the cloud.',
     'A special click event for this tab was bound in /client/default/js/init.js which causes a $fh.act() request to be made each time the tab is clicked.',
     'Look at the file /cloud/main.js (configCall() function)to see it reading the "cloudData" key from the /shared/config.js file.'
